7fd56082c9225613e648321b35ffd30daa519177
lingkai <lingkaizhaolkz@gmail.com>
2020-09-28 03:25:56 -0400

    new

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 //Install express server
 const express = require("express");
 const path = require("path");
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
