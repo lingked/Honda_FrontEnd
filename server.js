07220486d98ceb4cdc01387cf0b45e746dcd6c28
lingkai <lingkaizhaolkz@gmail.com>
2020-09-28 04:50:40 -0400

    init

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ const app = express();
 
 app.use(cors());
 
-// app.use((req, res, next) => {
+// app.use(function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
 //   next();
 // });
 
