6dcbe6f74b89382915000d4d5a0cad8c5fa0d30d
lingkai <lingkaizhaolkz@gmail.com>
2020-09-28 03:09:05 -0400

    cors

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const app = express();
 
 // app.use(cors());
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + "/dist/HondaFront"));
