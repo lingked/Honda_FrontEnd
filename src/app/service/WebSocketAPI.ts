d4ea30ce0521b91b4cc75bf2914b200054ff08af
lingkai <lingkaizhaolkz@gmail.com>
2020-09-28 04:57:09 -0400

    new

--- src/app/service/WebSocketAPI.ts
+++ src/app/service/WebSocketAPI.ts
@@ -5,8 +5,8 @@ import { AppComponent } from '../app.component';
 
 export class WebSocketAPI {
   // WebSocketEndPoint: string = 'http://localhost:8080/realTimeAPI';
-  WebSocketEndPoint: string = 'http://3.22.61.171:8080/realTimeAPI';
-  // WebSocketEndPoint: string = 'https://honda-api-demo.herokuapp.com';
+  // WebSocketEndPoint: string = 'http://3.22.61.171:8080/realTimeAPI';
+  WebSocketEndPoint: string = 'https://honda-api-demo.herokuapp.com';
   topic: string = '/dataListener';
   stompClient: any;
   appComponent: AppComponent;
