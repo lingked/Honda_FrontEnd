c368431208d25f7b13903d6ea4b7571965cfb3e5
lingkai <lingkaizhaolkz@gmail.com>
2020-09-28 03:37:19 -0400

    new

--- server.js
+++ server.js
@@ -5,7 +5,20 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Origin",
+      "x-access-token",
+      "XSRF-TOKEN",
+    ],
+    preflightContinue: false,
+  })
+);
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
