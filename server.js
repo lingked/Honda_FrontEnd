27862e526b834d0c707fd3213b50c498d6b56cf3
lingkai <lingkaizhaolkz@gmail.com>
2020-09-28 02:38:08 -0400

    cors

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors);
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + "/dist/HondaFront"));
