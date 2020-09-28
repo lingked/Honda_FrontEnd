4606ef1ac36e70ba8104c8063a9021ec67ba9e00
lingkai <lingkaizhaolkz@gmail.com>
2020-09-28 03:15:19 -0400

    cors

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
